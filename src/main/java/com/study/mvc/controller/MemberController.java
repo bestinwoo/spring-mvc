5b6db28766bae111e740fa1b9820e556f14e03d1
bestinwoo <bestinwoo@gmail.com>
2022-06-14 00:32:43 +0900

    OAuth 2.0 구글 로그인 DB 연동 및 html 연동

--- src/main/java/com/study/mvc/controller/MemberController.java
+++ src/main/java/com/study/mvc/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.study.mvc.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,5 +20,11 @@ public class MemberController {
     @GetMapping("/login/oauth2/code/google")
     public String googleLogin() {
         return "/";
+    }
+
+    @GetMapping("/member/loginInfo")
+    @ResponseBody
+    public String loginInfo(Authentication authentication, @AuthenticationPrincipal Principal principal) {
+        return "hi";
     }
 }
